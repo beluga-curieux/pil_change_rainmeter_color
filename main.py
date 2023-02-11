d8f11e718f34f0234668051cef9ef1152081a934
belug <beluga.curieux@gmail.com>
2023-02-11 22:20:17 +0100

    ajout de la fonction void

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-
 import PIL.Image
+
+img = PIL.Image.open("originale.png")
 
 
 def get_brightness(c: tuple) -> int:
@@ -11,14 +11,14 @@ def get_brightness(c: tuple) -> int:
     return int(s/3)
 
 
-def main():
+def void() -> PIL.Image.Image:
+    return PIL.Image.new("RGBA", img.size)
 
-    img = PIL.Image.open("Background.png")
+
+def recolorize(new_color=(29, 40, 50)) -> PIL.Image.Image:
     result = img.copy()
 
     w, h = result.size
-
-    new_color = 29, 40, 50
 
     for x in range(w):
         for y in range(h):
@@ -26,12 +26,19 @@ def main():
             value = img.getpixel((x, y))
 
             if value[3] > 100 and get_brightness(value) < 50:
-
                 result.putpixel((x, y), (*new_color, value[3]))
 
-    result.show()
+    return result
 
-    result.save("result.png")
+
+def main():
+    void_img = void()
+    recolor_img = recolorize()
+
+    new = void_img
+
+    new.show()
+    new.save("result.png")
 
 
 if __name__ == '__main__':
